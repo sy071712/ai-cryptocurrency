82966cad2dd8feddcdbf043a558d80f2c0460c36
sy071712 <166480004+sy071712@users.noreply.github.com>
2024-06-11 13:54:14 +0900

    Update ai-crypto-project-3-lasso.r

--- ai-crypto-project-3-lasso.r
+++ ai-crypto-project-3-lasso.r
@@ -1,38 +1,44 @@
 library('stringr')
 library('glmnet')
-
-# 함수 정의
 extract <- function(o, s) { 
-  index <- which(coef(o, s) != 0) 
-  data.frame(name = rownames(coef(o))[index], coef = coef(o, s)[index]) 
+ index <- which(coef(o, s) != 0) 
+ data.frame(name=rownames(coef(o))[index], coef=coef(o, s)[index]) 
 }
-
-options(scipen = 999)
-
-# 파일 이름 설정
-filtered <- "2024-05-01T000000-2024-05-01T235900-upbit-BTC-filtered-5-2-mid5.csv"
-model_file <- "2024-05-01T235900-upbit-BTC-lasso-5s-2std.csv"
-
-# 데이터 로드 및 전처리
-if (file.exists(filtered)) {  # 파일이 존재하는지 확인
-  filtered <- read.csv(filtered)
-  mid_std <- sd(filtered$mid_price)
-  message(round(mid_std, 0))
-
-  filtered_no_time_mid <- subset(filtered, select = -c(mid_price, timestamp))
-
-  y <- filtered_no_time_mid$return
-  x <- subset(filtered_no_time_mid, select = -c(return))
-
-  x <- as.matrix(x)
-
-  # Lasso 회귀 모델 학습
-  cv_fit <- cv.glmnet(x = x, y = y, alpha = 1, intercept = FALSE, lower.limits = 0, nfolds = 5) # lasso
-
-  fit <- glmnet(x = x, y = y, alpha = 1, lambda = cv_fit$lambda.1se, intercept = FALSE, lower.limits = 0)
-
-  # 모델 계수 추출 및 저장
-  df <- extract(fit, s = cv_fit$lambda.1se)
-  df <- t(df)
-  write.table(df, file = model_file, sep = ",", col.names = FALSE, row.names = FALSE, quote = FALSE)
-}
+options(scipen=999)
+args<-commandArgs(TRUE)
+#args[1] s time
+#args[2] e time
+#args[3] exchange
+#args[4] 
+filtered = paste(args[1],args[2],args[3],args[4],'filtered-5-2',args[5],sep="-")
+model_file = paste(args[2],args[3],args[4],args[5],'lasso-5s-2std',sep='-')
+#return_file
+filtered <- str_remove_all(filtered,":")
+model_file <- str_remove_all(model_file,":")
+filtered = paste ("./", filtered, ".csv", sep="")
+message(filtered)
+message(model_file)
+model_file = paste ("./", model_file, ".csv", sep="")
+filtered = read.csv("2024-05-01T000000-2024-05-01T235900-upbit-BTC-filtered-5-2-
+mid5.csv")
+mid_std = sd(filtered$mid_price)
+message (round(mid_std,0))
+#print (round(mid_std[1],0))
+#print (mid_std)
+filtered_no_time_mid = subset(filtered, select=-c(mid_price,timestamp))
+y = filtered_no_time_mid$return
+x = subset(filtered_no_time_mid, select=-c(return))
+#quit()
+x<-as.matrix(x)
+#model_ols<-lm(y~x)
+#model_lasso<-glmnet(x,y)
+#cv_fit <- cv.glmnet(x=x, y=y, alpha=0, intercept=FALSE, lower.limits=0, nfolds=10) 
+#ridge
+cv_fit <- cv.glmnet(x=x, y=y, alpha=1, intercept=FALSE, lower.limits=0, nfolds=5) #lasso
+fit <- glmnet(x=x, y=y, alpha = 1, lambda=cv_fit$lambda.1se, intercept=FALSE, 
+lower.limits=0,)
+#coef(fit)
+df <- extract(fit, s=0.1)
+df <- t(df)
+write.table(df, file=model_file, sep=",", col.names=FALSE, row.names=FALSE, 
+quote=FALSE
